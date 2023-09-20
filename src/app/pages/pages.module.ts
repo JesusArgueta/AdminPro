234617cbe759f1a39af643c075f10e550b026d35
Argueta <argueta03@hotmail.com>
2023-09-20 17:40:01 +0200

    Seccion 6

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -7,6 +7,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { ProgressComponent } from './progress/progress.component';
 import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { ComponentsModule } from '../components/components.module';
 
 
 
@@ -21,7 +23,9 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule
+    RouterModule,
+    FormsModule,
+    ComponentsModule
   ],
   exports:[
     DashboardComponent,
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,9 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
+  styleUrls: [ './progress.component.css'
   ]
 })
 export class ProgressComponent {
+
+  progreso1: number = 25;
+  progreso2: number = 35;
+
+  getProgreso1():string{
+    return `${this.progreso1}%`;
+  }
+
+  getProgreso2():string{
+    return `${this.progreso2}%`;
+  }
 
 }
